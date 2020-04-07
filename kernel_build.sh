0e7b4fe2426616c6fa5dd0ebd915cf4f3b690670
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-04-07 21:42:35 +0200

    script: too much improves
    * Read script comments for more informations

--- kernel_build.sh
+++ kernel_build.sh
@@ -9,17 +9,21 @@ ANYKERNEL_DIR=$ROOT_DIR/anykernel3
 KERNEL_DIR=$ROOT_DIR/kernel
 DATE=$(date +"%m-%d-%y")
 BUILD_START=$(date +"%s")
+
 # Export ARCH and SUBARCH <arm, arm64, x86, x86_64>
-export ARCH=arm
-export SUBARCH=arm
-# Set kernel name
-export LOCALVERSION=SebaUbuntu-Stock-U5
-# Export Username
+export ARCH=arm64
+export SUBARCH=arm64
+
+# Set kernel name and defconfig
+export VERSION=SebaUbuntu-KernelName-v1
+export DEFCONFIG=whyred_defconfig
+
+# Keep it as is
+export LOCALVERSION=-$VERSION
+
+# Export Username and machine name
 export KBUILD_BUILD_USER=SebaUbuntu
-# Export Machine name
 export KBUILD_BUILD_HOST=Seba-PC
-# Export what defconfig you want to use
-export DEFCONFIG=j4primelte_sea_open_defconfig
 
 # Color definition
 red=`tput setaf 1`
@@ -31,29 +35,28 @@ cyan=`tput setaf 6`
 white=`tput setaf 7`
 reset=`tput sgr0`
 
-# Set kernel source workspace
-cd $KERNEL_DIR
-
-# Compiler String
+# Cross-compiler exporting
 if [ $ARCH = arm ]
 	then
 		# Export ARM from the given directory
 		export CROSS_COMPILE=${ROOT_DIR}/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-	elif [ $ARCH = arm64 ]
-		then
-			# Export ARM64 and ARM cross-compliers from the given directory
-			export CROSS_COMPILE=${ROOT_DIR}/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-			export CROSS_COMPILE_ARM32=${ROOT_DIR}/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-# x86_32 toolchain wrong, please change it manually
-	elif [ $ARCH = x86 ]
-		then
-			# Export ARM64 and ARM cross-compliers from the given directory
-			export CROSS_COMPILE=${ROOT_DIR}/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/bin/x86_64-linux-android-
-	elif [ $ARCH = x86_64 ]
-		then
-			# Export ARM64 and ARM cross-compliers from the given directory
-			export CROSS_COMPILE=${ROOT_DIR}/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/bin/x86_64-linux-android-
-			export CROSS_COMPILE_X86=${ROOT_DIR}/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/bin/x86_64-linux-android-
+elif [ $ARCH = arm64 ]
+	then
+		# Export ARM64 and ARM cross-compliers from the given directory
+		export CROSS_COMPILE=${ROOT_DIR}/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+		export CROSS_COMPILE_ARM32=${ROOT_DIR}/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+elif [ $ARCH = x86 ]
+	then
+		# Export x86 cross-compliers from the given directory
+		export CROSS_COMPILE=${ROOT_DIR}/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/bin/x86_64-linux-android-
+elif [ $ARCH = x86_64 ]
+	then
+		# Export x86 and x86_64 cross-compliers from the given directory
+		export CROSS_COMPILE=${ROOT_DIR}/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/bin/x86_64-linux-android-
+		export CROSS_COMPILE_X86=${ROOT_DIR}/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/bin/x86_64-linux-android-
+else
+	echo "$red Error: Arch not compatible $reset"
+	exit
 fi
 
 echo -e "*****************************************************"
@@ -62,51 +65,117 @@ echo -e "*****************************************************"
 echo -e "-----------------------------------------------------"
 echo    " Architecture: $ARCH                                 "
 echo    " Output directory: $OUT_DIR                          "
-echo    " Kernel version: $LOCALVERSION                       "
+echo    " Kernel version: $VERSION                            "
 echo    " Build user: $KBUILD_BUILD_USER                      "
 echo    " Build machine: $KBUILD_BUILD_HOST                   "
 echo    " Build started on: $BUILD_START                      "
-echo    " Toolchain: GCC                                      "
+echo    " Toolchain: GCC 4.9                                  "
 echo -e "-----------------------------------------------------"
 
-# Make and Clean
+# Set kernel source workspace
+cd $KERNEL_DIR
+
+# Clean build
 make O=$OUT_DIR clean
+CLEAN_SUCCESS=$?
+if [ $CLEAN_SUCCESS != 0 ]
+	then
+		echo "$red Error: make clean failed"
+		exit
+fi
+
 make O=$OUT_DIR mrproper
+MRPROPER_SUCCESS=$?
+if [ $MRPROPER_SUCCESS != 0 ]
+	then
+		echo "$red Error: make mrproper failed"
+		exit
+fi 
 
 # Make your device device_defconfig
 make O=$OUT_DIR ARCH=$ARCH $DEFCONFIG
+DEFCONFIG_SUCCESS=$?
+if [ $DEFCONFIG_SUCCESS != 0 ]
+	then
+		echo "$red Error: make $DEFCONFIG failed, specified a defconfig not present? $reset"
+		exit
+fi
 
 # Build Kernel
 make O=$OUT_DIR ARCH=$ARCH -j$(nproc --all)
-# Save build result (failed or not)
-BUILD_SUCCESS=$?
+
 # Find how much build has been long
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 
-if [ $BUILD_SUCCESS = 0 ]
+BUILD_SUCCESS=$?
+if [ $BUILD_SUCCESS != 0 ]
 	then
-		clear
-		echo -e "$green Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds. $reset"
-		echo -e "Making Flashable Template using anykernel3"
-		# Clean anykernel2 directory
-		rm -f ${ANYKERNEL_DIR}/Image.gz*
-		rm -f ${ANYKERNEL_DIR}/zImage*
-		rm -f ${ANYKERNEL_DIR}/dtb*
-		# Change the directory to anykernel3 directory
-		cd ${ANYKERNEL_DIR}
-		# remove all zips
-		rm *.zip
-		# Copy thhe image.gz-dtb to anykernel2 directory
-		cp $OUT_DIR/arch/$ARCH/boot/zImage-dtb ${ANYKERNEL_DIR}/zImage
-		# Build a flashable zip Device using anykernel2
-		zip -r9 kernel-$LOCALVERSION-$DATE.zip * -x README kernel-$LOCALVERSION-$DATE.zip
-	else
-		echo -e "$red Build failed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds. $reset"
+		echo "$red Error: Build failed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds $reset"
+		exit
 fi
 
+# Possible kernel filenames
+APPENDED_DTB_KERNEL="zImage-dtb Image.gz-dtb Image.bz2-dtb Image.lzo-dtb Image.lzma-dtb Image.xz-dtb Image.lz4-dtb Image-dtb"
+NOT_APPENDED_DTB_KERNEL="zImage Image.gz Image.bz2 Image.lzo Image.lzma Image.xz Image.lz4 Image.fit Image"
+KERNEL_DTB="dt dt.img dtb dtb.img"
+
+cd "$ROOT_DIR"
+
+clear
+echo -e "$green Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds $reset"
+echo -e "Making flashable zip using anykernel3"
+
+# remove all zips and old kernels
+rm "$ANYKERNEL_DIR/*.zip"
+for i in "$APPENDED_DTB_KERNEL $NOT_APPENDED_DTB_KERNEL $KERNEL_DTB"
+	do
+		if [ -f "$ANYKERNEL_DIR/$i" ]
+			then
+				rm -f "$ANYKERNEL_DIR/$i"
+		fi
+done
+
+# First of all see if kernel with appended dtb exists and copy it
+for i in "$APPENDED_DTB_KERNEL"
+	do
+		if [ -f "$OUT_DIR/arch/$ARCH/boot/$i" ]
+			then
+				cp "$OUT_DIR/arch/$ARCH/boot/$i" "$ANYKERNEL_DIR/$i"
+				KERNEL_HAVE_APPENDED_DTB=true
+		fi
+		[ $KERNEL_HAVE_APPENDED_DTB ] && break
+done
+
+# If kernel with appended dtbs doesn't exists, copy kernel w/o appended dtb and dtb image
+if [ "$KERNEL_HAVE_APPENDED_DTB" != true ]
+	then
+		for i in $NOT_APPENDED_DTB_KERNEL
+			do
+				if [ -f "$OUT_DIR/arch/$ARCH/boot/$i" ]
+					then
+						cp "$OUT_DIR/arch/$ARCH/boot/$i" "$ANYKERNEL_DIR/$i"
+						KERNEL_HAVE_APPENDED_DTB=false
+				fi
+				[ $KERNEL_HAVE_APPENDED_DTB = false ] && break
+		done
+		for i in $KERNEL_DTB
+			do
+				if [ -f "$OUT_DIR/arch/$ARCH/boot/$i" ]
+					then
+						cp "$OUT_DIR/arch/$ARCH/boot/$i" "$ANYKERNEL_DIR/$i"
+				fi
+		done
+fi
+
+# Change the directory to anykernel3 directory
+cd $ANYKERNEL_DIR
+	
+# Build a flashable zip Device using anykernel2
+zip -r9 $VERSION-$DATE.zip $ANYKERNEL_DIR/* -x $ANYKERNEL_DIR/README $ANYKERNEL_DIR/$VERSION-$DATE.zip
+
 # Exported variables cleanup
-for i in O ARCH SUBARCH LOCALVERSION KBUILD_BUILD_USER KBUILD_BUILD_HOST DEFCONFIG CROSS_COMPILE CROSS_COMPILE_ARM32 CROSS_COMPILE_X86
+for i in ARCH SUBARCH LOCALVERSION KBUILD_BUILD_USER KBUILD_BUILD_HOST DEFCONFIG CROSS_COMPILE CROSS_COMPILE_ARM32 CROSS_COMPILE_X86
 	do
 		unset $i
-	done
+done
